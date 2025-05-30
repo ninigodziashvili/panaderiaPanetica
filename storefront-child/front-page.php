50a488d80b4c16c28f9f8e4176203615d3828d4c
ninigodziashvili <nino.godziashvili@gmail.com>
2025-05-30 13:46:12 +0200

    comiit for the bread shop

--- storefront-child/front-page.php
+++ storefront-child/front-page.php
@@ -11,7 +11,7 @@
 
 <section class="about-section">
   <div class="about-content">
-    <h2 class="about-title">Sobre Nosotros</h2>
+  <!--   <h2 class="about-title">Sobre Nosotros</h2> -->
     <p class="about-text">
       En Pan Natural, creemos en el poder de los ingredientes puros. Cada pieza de pan es horneada con dedicación, utilizando recetas tradicionales y productos 100% orgánicos. Nuestro compromiso es ofrecer un sabor auténtico que nutre tanto el cuerpo como el alma.
     </p>
