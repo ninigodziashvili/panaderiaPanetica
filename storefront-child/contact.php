a8afa02acfc11766bd188d1de56f5ac00777dbae
ninigodziashvili <nino.godziashvili@gmail.com>
2025-06-02 12:03:23 +0200

    logo and some changes

---
+++ storefront-child/contact.php
@@ -0,0 +1,77 @@
+<?php
+// Template Name: Contacto
+?>
+<?php get_header(); ?>
+<div class="contact_us_6">
+  <div class="responsive-container-block container">
+    <form class="form-box">
+      <div class="container-block form-wrapper">
+        <div class="mob-text">
+          <p class="text-blk contactus-head">
+            Contáctanos
+          </p>
+          <p class="text-blk contactus-subhead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis diam lectus sapien.
+          </p>
+        </div>
+        <div class="responsive-container-block" id="i2cbk">
+          <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i10mt-3">
+            <p class="text-blk input-title">
+             NOMBRE
+            </p>
+            <input class="input" id="ijowk-3" name="FirstName" placeholder="Por favor, introduzca su nombre...">
+          </div>
+          <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="ip1yp">
+            <p class="text-blk input-title">
+              CORREO ELECTRÓNICO
+            </p>
+            <input class="input" id="ipmgh-3" name="Email" placeholder="Por favor ingrese su correo electrónico...">
+          </div>
+          <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="ih9wi">
+            <p class="text-blk input-title">
+             NÚMERO DE TELÉFONO
+            </p>
+            <input class="input" id="imgis-3" name="PhoneNumber" placeholder="Por favor ingrese el número de teléfono...">
+          </div>
+          <div class="responsive-cell-block wk-tab-12 wk-mobile-12 wk-desk-12 wk-ipadp-12" id="i634i-3">
+            <p class="text-blk input-title">
+              ¿QUÉ TIENES EN MENTE?
+            </p>
+            <textarea class="textinput" id="i5vyy-3" placeholder="Por favor ingrese su consulta..."></textarea>
+          </div>
+        </div>
+        <button class="submit-btn" id="w-c-s-bgc_p-1-dm-id-2">
+          Entregar
+        </button>
+      </div>
+    </form>
+    <div class="responsive-cell-block wk-desk-7 wk-ipadp-12 wk-tab-12 wk-mobile-12" id="i772w">
+      <div class="map-part">
+        <p class="text-blk map-contactus-head" id="w-c-s-fc_p-1-dm-id">
+          Contáctanos en
+        </p>
+        <p class="text-blk map-contactus-subhead">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Felis diam lectus sapien.
+        </p>
+        <div class="social-media-links mob">
+          <a class="social-icon-link" href="#" id="ix94i-2-2">
+            <img class="link-img image-block" src="https://img.freepik.com/vector-gratis/nuevo-diseno-icono-x-logotipo-twitter-2023_1017-45418.jpg?semt=ais_items_boosted&w=740">
+          </a>
+          <a class="social-icon-link" href="#" id="itixd">
+            <img class="link-img image-block" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Facebook_icon.svg/2048px-Facebook_icon.svg.png">
+          </a>
+          <a class="social-icon-link" href="#" id="izxvt">
+            <img class="link-img image-block" src="https://static-00.iconduck.com/assets.00/discord-icon-256x256-9roejvqx.png">
+          </a>
+          <a class="social-icon-link" href="#" id="izldf-2-2">
+            <img class="link-img image-block" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/1024px-Instagram_icon.png">
+          </a>
+        </div>
+        <div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d23404.110359826245!2d-2.670592!3d42.84088319999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses!2ses!4v1748850508549!5m2!1ses!2ses" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<?php get_footer(); ?>
--- storefront-child/functions.php
+++ storefront-child/functions.php
@@ -10,3 +10,38 @@ function my_function_admin_bar(){
 }
 add_filter( 'show_admin_bar' , 'my_function_admin_bar');
 
+// javascript
+
+function mi_tema_scripts() {
+ wp_enqueue_script(
+ 'main-js',
+ get_template_directory_uri() . '/assets/js/main.js',
+ array(),
+ null,
+ true
+ );
+}
+add_action('wp_enqueue_scripts', 'mi_tema_scripts');
+
+
+// Remove default Storefront branding output
+remove_action( 'storefront_header', 'storefront_site_branding', 20 );
+
+// Add your custom branding with logo inside the same div
+function custom_storefront_site_branding() {
+    ?>
+    <div class="site-branding-One">
+        <a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home" style="display: flex; align-items: center;">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/file.png" alt="<?php bloginfo( 'name' ); ?>" style="max-height: 130px; margin-right: 15px;" />
+            <h1 class="site-title" style="margin: 0;"><?php bloginfo( 'name' ); ?></h1>
+        </a>
+        <?php
+        $description = get_bloginfo( 'description', 'display' );
+        if ( $description || is_customize_preview() ) :
+            ?>
+            <p class="site-description"><?php echo $description; ?></p>
+        <?php endif; ?>
+    </div>
+    <?php
+}
+add_action( 'storefront_header', 'custom_storefront_site_branding', 20 );
